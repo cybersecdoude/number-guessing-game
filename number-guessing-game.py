4bbbd7a50b079711d79c9da22ef94c6ba9925426
cybersecdoude <cybersecdoude@gmail.com>
2025-08-14 19:22:43 +0200

    C: Version 2.0 of Number Guessing Game

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,15 +1,20 @@
 import random
 
 random_number = random.randint(0, 100)
+num_of_guesses = 0
 
 while True:
-    guess = int(input("Guess a number from 0 to 100: "))
+    try:
+        guess = int(input("Guess a number from 0 to 100: "))
+    except ValueError:
+        print("You entered an invalid number, please try again.")
+        continue
+    num_of_guesses += 1
     if guess == random_number:  
-        print(f"You guessed correctly! The number was {random_number}")
+        print(f"You guessed correctly after {num_of_guesses} attempt(s)! The number was {random_number}")
         break
     else:
         if guess > random_number:
             print("Too high!")
         else:
             print("Too low!")
-
