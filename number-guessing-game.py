18ecf29cc7ea13c3df67daa8ca9f9bab5d564072
cybersecdoude <cybersecdoude@gmail.com>
2025-08-14 15:25:00 +0200

    B: Version 1.0 of Number Guessing Game

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,1 +1,15 @@
-# placeholder
+import random
+
+random_number = random.randint(0, 100)
+
+while True:
+    guess = int(input("Guess a number from 0 to 100: "))
+    if guess == random_number:  
+        print(f"You guessed correctly! The number was {random_number}")
+        break
+    else:
+        if guess > random_number:
+            print("Too high!")
+        else:
+            print("Too low!")
+
